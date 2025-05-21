d872edface96561b50a9e615068d783ddb8349c8
Ghabriel-Silva <ghaabriieel@gmail.com>
2025-05-20 22:16:45 -0300

    Iniciando projeto, e instalei o tawiland

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //Estado para mostrar a prioridade
+  const [prioridade, setPrioridade] = useState('')
+
+  //Executa 1 vez, faz referencia para o Input inicial
+  const refInput = useRef(null)
+  useEffect(()=>{
+    refInput.current.focus()
+  }, [])
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <h1>Add Task</h1>
+          <p>Numero</p>
+        </div>
+
+        <div>
+          <input 
+            type="text" 
+            placeholder='Task...' 
+            ref={refInput}
+          />
+          <div clas>
+            <label htmlFor="select"  >Prioridade</label>
+            <select id='select' value={prioridade} onChange={(e) => setPrioridade(e.target.value)} >
+              <option value="" disabled hidden>Priority</option>
+              <option value="alta">High </option>
+              <option value="normal"> Normal</option>
+              <option value="baixa">Low </option>
+            </select>
+            <button>add</button>
+          </div>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
