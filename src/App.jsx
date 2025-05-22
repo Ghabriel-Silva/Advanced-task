61e2c05e8853657e1571e18d5554ae121c7672e3
Ghabriel-Silva <ghaabriieel@gmail.com>
2025-05-21 22:03:44 -0300

    adicionando layout usando o chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,56 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
+import { Input, Box, Flex, Heading, Text, Button, NativeSelect } from "@chakra-ui/react"
+
+
 
 function App() {
 
   //Estado para mostrar a prioridade
-  const [prioridade, setPrioridade] = useState('')
+  const  [prioridade, setPrioridade ] = useState('')
 
   //Executa 1 vez, faz referencia para o Input inicial
   const refInput = useRef(null)
-  useEffect(()=>{
+  useEffect(() => {
     refInput.current.focus()
   }, [])
 
   return (
-      <div>
-        <div>
-          <h1>Add Task</h1>
-          <p>Numero</p>
-        </div>
-
-        <div>
-          <input 
-            type="text" 
-            placeholder='Task...' 
-            ref={refInput}
-          />
-          <div clas>
-            <label htmlFor="select"  >Prioridade</label>
-            <select id='select' value={prioridade} onChange={(e) => setPrioridade(e.target.value)} >
-              <option value="" disabled hidden>Priority</option>
+    <Box as="main" h="90vh"  maxW="680px" display="flex" flexDirection="column" alignItems="center" margin="auto">
+      <Flex gap="4" justify="space-between" w="100%"  justifyItems="center"  pl="16px" pr="16px" >
+        <Heading pl="3" fontSize={{base: 'sm', md: 'md', lg:'2xl'}}>Add Task</Heading>
+        <Text pr="3" textStyle="md">Numero</Text>
+      </Flex>
+      <Flex h="10vh" gap="4" justify="space-evenly" align="center" wrap="wrap">
+        <Input w="sm"
+          h="36px"
+          type="text"
+          placeholder='Task...'
+          ref={refInput}
+        />
+        <Flex direction="row" gap="4"  wrap="revert">
+          <NativeSelect.Root   value={prioridade} onChange={(e) => setPrioridade(e.target.value)} >
+            <NativeSelect.Field placeholder="Priority" h="36px" w="130px" >
               <option value="alta">High </option>
               <option value="normal"> Normal</option>
               <option value="baixa">Low </option>
-            </select>
-            <button>add</button>
-          </div>
-        </div>
+            </NativeSelect.Field>
+            <NativeSelect.Indicator />
+          </NativeSelect.Root>
+          <Button
+            variant="solid"
+            h="34px"
+            w="80px"
+            colorScheme="blue"
+          >
+            add
+          </Button>
 
-      </div>
+        </Flex>
+
+      </Flex>
+
+    </Box>
   )
 }
 
